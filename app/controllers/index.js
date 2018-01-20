384963b43c3cc812a8f5ee15e9365530fd993b16
Kurtis Houser <houserkurtis@gmail.com>
2018-01-20 09:53:29 -0800

    Remove redundant routes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,6 @@
 function index(req, res) {
   res.send({
-    markers: 'http://localhost:5000/markers',
-    regions: 'http://localhost:5000/regions',
     mapDatasetsUrl: 'http://localhost:5000/map-datasets',
-    communityResiliencyIndicators: 'http://localhost:5000/community-resiliency-indicators'
   });
 }
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const markers = require('./markers');
-const regions = require('./regions');
 const mapDatasets = require('./mapDatasets');
 const index = require('../controllers');
-const communityResiliencyIndicators = require('../controllers/community-resiliency-indicators');
 
 const router = express.Router();
 
-router.use('/markers', markers);
-router.use('/regions', regions);
 router.use('/map-datasets', mapDatasets);
 
 router.route('/')
   .get(index);
-
-router.route('/community-resiliency-indicators')
-  .get(communityResiliencyIndicators);
 
 module.exports = router;
