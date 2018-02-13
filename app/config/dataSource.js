c29ce3a2a20306fac65fa13cdfd5c89ad492b6aa
Kurtis Houser <houserkurtis@gmail.com>
2018-02-12 17:00:06 -0800

    Remove comment about the need to filter external data

--- app/config/dataSource.js
+++ app/config/dataSource.js
@@ -14,7 +14,6 @@ exports.external = {
   communityResiliencyIndicators: 'https://data.sfgov.org/resource/wsj2-27m9.json',
   /* --- SF Open Data Markers --- */
   privatelyOwnedPublicOpenSpaces: 'https://data.sfgov.org/resource/3ub7-d4yy.geojson?$where=the_geom IS NOT NULL',
-  // many records do not have lat & lng, need to filter these out
   parksAndOpenSpaces: 'https://data.sfgov.org/resource/94uf-amnx.geojson?$where=location_1 IS NOT NULL',
   schools: 'https://data.sfgov.org/resource/mmsr-vumy.geojson?$where=location_1 IS NOT NULL',
   // very large, nation-wide data set, need to limit what's returned
