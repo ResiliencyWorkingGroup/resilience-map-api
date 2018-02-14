3f47a3490ece4f303ba6e1d4effb6c7aa867aa0f
Kurtis Houser <kurtishouser@users.noreply.github.com>
2018-02-14 07:05:29 -0800

    Filter remote data & lock dependency versions (#2)
    * Update dependencies and lock their versions
    
    * Update DataSF API URLs to filter out useless data
    
    * Update description in package.json
    
    * Remove comment about the need to filter external data

--- app/config/dataSource.js
+++ app/config/dataSource.js
@@ -13,16 +13,15 @@ exports.external = {
   /* --- SF Open Data --- */
   communityResiliencyIndicators: 'https://data.sfgov.org/resource/wsj2-27m9.json',
   /* --- SF Open Data Markers --- */
-  privatelyOwnedPublicOpenSpaces: 'https://data.sfgov.org/resource/3ub7-d4yy.geojson',
-  // many records do not have lat & lng, need to filter these out
-  parksAndOpenSpaces: 'https://data.sfgov.org/resource/94uf-amnx.geojson',
-  schools: 'https://data.sfgov.org/resource/mmsr-vumy.geojson',
+  privatelyOwnedPublicOpenSpaces: 'https://data.sfgov.org/resource/3ub7-d4yy.geojson?$where=the_geom IS NOT NULL',
+  parksAndOpenSpaces: 'https://data.sfgov.org/resource/94uf-amnx.geojson?$where=location_1 IS NOT NULL',
+  schools: 'https://data.sfgov.org/resource/mmsr-vumy.geojson?$where=location_1 IS NOT NULL',
   // very large, nation-wide data set, need to limit what's returned
-  businesses: 'https://data.sfgov.org/resource/vbiu-2p9h.geojson',
-  cityFacilities: 'https://data.sfgov.org/resource/i5wr-crji.geojson',
-  healthCareFacilities: 'https://data.sfgov.org/resource/sci7-7q9i.geojson',
-  pitStops: 'https://data.sfgov.org/resource/snkr-6jdf.geojson',
+  businesses: 'https://data.sfgov.org/resource/vbiu-2p9h.geojson?$where=location IS NOT NULL',
+  cityFacilities: 'https://data.sfgov.org/resource/i5wr-crji.geojson?$where=geom IS NOT NULL',
+  healthCareFacilities: 'https://data.sfgov.org/resource/sci7-7q9i.geojson?$where=location IS NOT NULL',
+  pitStops: 'https://data.sfgov.org/resource/snkr-6jdf.geojson?$where=geom IS NOT NULL',
   /* --- SF Open Data Regions --- */
-  seizmicHazardZones: 'https://data.sfgov.org/resource/t2cc-dy6b.geojson',
-  neighborhoods: 'https://data.sfgov.org/resource/6ia5-2f8k.geojson',
+  seizmicHazardZones: 'https://data.sfgov.org/resource/t2cc-dy6b.geojson?$where=the_geom IS NOT NULL',
+  neighborhoods: 'https://data.sfgov.org/resource/6ia5-2f8k.geojson?$where=the_geom IS NOT NULL',
 };
