e3c0eaf8a02a17c5c6d78c3909db0e8a19fdfbc2
Jeff Appareti <jappareti@gmail.com>
2018-03-07 17:18:16 -0800

    Updated SOQL query strings for DataSF sources

--- app/config/dataSource.js
+++ app/config/dataSource.js
@@ -12,76 +12,66 @@ exports.internal = {
 exports.external = {
   /* --- SF Open Data --- */
   communityResiliencyIndicators: {
-    url:'https://data.sfgov.org/resource/wsj2-27m9.json',
-    modifyDataset: (dataset) => {
+    url: "https://data.sfgov.org/resource/wsj2-27m9.json",
+    modifyDataset: dataset => {
       // do something here as needed
       return dataset;
-    },
+    }
   },
   /* --- SF Open Data Markers --- */
   privatelyOwnedPublicOpenSpaces: {
-    url: 'https://data.sfgov.org/resource/3ub7-d4yy.geojson?$where=the_geom IS NOT NULL',
-    modifyDataset: (dataset) => {
-      // do something here as needed
-      return dataset;
-    },
+    url: "https://data.sfgov.org/resource/3ub7-d4yy.geojson",
+    query:
+      "$query=SELECT name AS title, descriptio AS description, the_geom WHERE the_geom IS NOT NULL",
+    author: "DataSF"
   },
   parksAndOpenSpaces: {
-    url: 'https://data.sfgov.org/resource/94uf-amnx.geojson?$where=location_1 IS NOT NULL',
-    modifyDataset: (dataset) => {
-		  // do something here as needed
-      return dataset;
-	  },
+    url: "https://data.sfgov.org/resource/94uf-amnx.geojson",
+    query:
+      "$query=SELECT parkid AS _id, parktype AS description, parkname AS name, location_1 WHERE location_1 IS NOT NULL",
+    author: "DataSF"
   },
   schools: {
-  url: 'https://data.sfgov.org/resource/mmsr-vumy.geojson?$where=location_1 IS NOT NULL',
-  modifyDataset: (dataset) => {
-    // do something here as needed
-    return dataset;
+    url: "https://data.sfgov.org/resource/mmsr-vumy.geojson",
+    query:
+      "$query=SELECT location_1, campus_name AS name, campus_address AS description, county_fips AS _id WHERE location_1 IS NOT NULL",
+    author: "DataSF"
   },
-},
   // very large, nation-wide data set, need to limit what's returned
   businesses: {
-    url: 'https://data.sfgov.org/resource/vbiu-2p9h.geojson?$where=location IS NOT NULL',
-    modifyDataset: (dataset) => {
-      // do something here as needed
-      return dataset;
-    },
+    url: "https://data.sfgov.org/resource/vbiu-2p9h.geojson",
+    query:
+      "$query=SELECT * WHERE location IS NOT NULL AND city = 'San Francisco'",
+    author: "DataSF"
   },
   cityFacilities: {
-    url: 'https://data.sfgov.org/resource/i5wr-crji.geojson?$where=geom IS NOT NULL',
-    modifyDataset: (dataset) => {
-      // do something here as needed
-      return dataset;
-    },
+    url: "https://data.sfgov.org/resource/i5wr-crji.geojson",
+    query:
+      "$query=SELECT facility_id AS _id, common_name AS name, address AS description, geom WHERE geom IS NOT NULL",
+    author: "DataSF"
   },
   healthCareFacilities: {
-    url: 'https://data.sfgov.org/resource/sci7-7q9i.geojson?$where=location IS NOT NULL',
-    modifyDataset: (dataset) => {
-      // do something here as needed
-      return dataset;
-    },
+    url: "https://data.sfgov.org/resource/sci7-7q9i.geojson",
+    query:
+      "$query=SELECT uid AS _id, facility_name AS name, facility_type AS description WHERE location IS NOT NULL",
+    author: "DataSF"
   },
   pitStops: {
-    url:'https://data.sfgov.org/resource/snkr-6jdf.geojson?$where=geom IS NOT NULL',
-    modifyDataset: (dataset) => {
-      // do something here as needed
-      return dataset;
-    },
+    url: "https://data.sfgov.org/resource/snkr-6jdf.geojson",
+    query:
+      "$query=SELECT id AS _id, location AS name, facilitytype AS description, geom WHERE geom IS NOT NULL",
+    author: "DataSF"
   },
   /* --- SF Open Data Regions --- */
   seizmicHazardZones: {
-    url: 'https://data.sfgov.org/resource/t2cc-dy6b.geojson?$where=the_geom IS NOT NULL',
-    modifyDataset: (dataset) => {
-      // do something here as needed
-      return dataset;
-    },
+    url: "https://data.sfgov.org/resource/t2cc-dy6b.geojson",
+    query: "$query=SELECT id AS _id, the_geom WHERE the_geom IS NOT NULL",
+    author: "DataSF"
   },
-  neighborhoods:{
-    url: 'https://data.sfgov.org/resource/6ia5-2f8k.geojson?$where=the_geom IS NOT NULL',
-    modifyDataset: (dataset) => {
-      // do something here as needed
-      return dataset;
-    },
-  },
+  neighborhoods: {
+    url: "https://data.sfgov.org/resource/6ia5-2f8k.geojson",
+    query:
+      "$query=SELECT the_geom, name, link AS description WHERE the_geom IS NOT NULL",
+    author: "DataSF"
+  }
 };
--- app/repositories/index.js
+++ app/repositories/index.js
@@ -3,6 +3,7 @@ const geojson = require('geojson');
 const Marker = require('../models/marker.js');
 const mapDatasets = require('../config/mapDatasets');
 const { internal, external } = require('../config/dataSource');
+const addAuthor = require("../utilities/addAuthor");
 
 async function getAll() {
   const datasets = await mapDatasets; // simulated db call
@@ -25,10 +26,10 @@ async function getById(mapDataset) {
 
 async function fetchById(mapDataset) {
   try {
-    const response = await fetch(external[mapDataset].url);
+    const response = await fetch(`${external[mapDataset].url}?${external[mapDataset].query}`);
     const dataset = await response.json();
 
-    const modifiedDataset = external[mapDataset].modifyDataset(dataset);
+    const modifiedDataset = addAuthor(dataset, external[mapDataset].author);
 
     return modifiedDataset;
   } catch (error) {
